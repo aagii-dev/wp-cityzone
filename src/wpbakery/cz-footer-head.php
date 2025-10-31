7e73f44e689c674c468b8d7b9e38d9d0eadb6895
Aagii <aagii.dev@gmail.com>
2025-10-31 11:02:25 +0800

    fotoerdw

--- src/wpbakery/cz-footer-head.php
+++ src/wpbakery/cz-footer-head.php
@@ -53,14 +53,16 @@ if (!class_exists('vc_FooterText') && class_exists('WPBakeryShortCode')) {
 
             // HTML бүтээж буцаана
             ob_start(); ?>
-            <div class="row">
-                <div class="col-12 col-md-10 col-lg-7">
-                    <div class="footer-head">
+            <div class="footer-head">
+                <div class='container'>
+                    <div class="row">
+                        <div class="col-12 col-md-10 col-lg-8 offset-lg-2 offset-md-1">
                         <div class="d-flex flex-row align-items-center justify-content-between">
                             <div class="text">
                             <?php echo $mg_text; ?>
                             </div>
                             <a href="<?php echo $mg_link; ?>" ><span><?php echo $mg_link_text; ?></span></a>
+                        </div>
                         </div>
                     </div>
                 </div>
